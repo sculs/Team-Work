9c1b7987c8d19b27adeb5b937e404adfc51283aa
sculs <liusongscu@gmail.com>
2017-10-08 12:18:20 +0200

    ändrade lite i Suit.class

--- team/Card.java
+++ team/Card.java
@@ -1,32 +1,15 @@
 package team;
 
-/**
- * This class implement a card type
- *
- * @author marcog
- *
- */
+
 public class Card {
-    /**
-     * One of the four valid suits for this card
-     */
+
     private Suit mySuit;
 
-    /**
-     * The number of this card, where Ace: 1, Jack-King: 11-13
-     */
     private int myNumber;
 
+    /// Constructors //////////
     public Card() {
     }
-
-    /**
-     * Card constructor
-     *
-     * @param aSuit      the suit of the card
-     * @param aNumber    the number of the card
-     */
-
 
     public Card(Suit aSuit, int aNumber) {
 
@@ -39,81 +22,33 @@ public class Card {
             System.exit(1);
         }
     }
-    /**
-     * Return the number of the card
-     *
-     * @return the number
-     */
+
     public int getNumber() {
-
         return myNumber;
-
     }
 
     public String toString() { //String array is another option
 
         String numStr = "Error";
-
         switch (this.myNumber) {
 
-            case 1:
-                numStr = "Ace";
-                break;
-
-            case 2:
-                numStr = "Two";
-                break;
-
-            case 3:
-                numStr = "Three";
-                break;
-
-            case 4:
-                numStr = "Four";
-                break;
-
-            case 5:
-                numStr = "Five";
-                break;
-
-            case 6:
-                numStr = "Six";
-                break;
-
-            case 7:
-                numStr = "Seven";
-                break;
-
-            case 8:
-                numStr = "Eight";
-                break;
-
-            case 9:
-                numStr = "Nine";
-                break;
-
-            case 10:
-                numStr = "Ten";
-                break;
-
-            case 11:
-                numStr = "Jack";
-                break;
-
-            case 12:
-                numStr = "Queen";
-                break;
-
-            case 13:
-                numStr = "King";
-                break;
-
-
-
+            case 1: numStr = "A"; break;
+            case 2: numStr = "2"; break;
+            case 3: numStr = "3"; break;
+            case 4: numStr = "4"; break;
+            case 5: numStr = "5"; break;
+            case 6: numStr = "6"; break;
+            case 7: numStr = "7"; break;
+            case 8: numStr = "8";break;
+            case 9: numStr = "9"; break;
+            case 10: numStr = "10"; break;
+            case 11: numStr = "J"; break;
+            case 12: numStr = "Q"; break;
+            case 13: numStr = "K";break;
 
         }
 
-        return "    " + numStr + " of " + mySuit.toString();
+        return "    " + mySuit.toString() + " " + numStr;
     }
 
 }
